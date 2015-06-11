eb77a1dd787c08b5de73dba43e5bec6318c8f9c2
chanwook <chanwook.god@gmail.com>
2015-06-11 13:09:33 +0900

    score 내림차순으로 상품이 나오도록 수정함

--- applestore-front/src/main/java/applestore/front/catalog/service/CatalogServiceImpl.java
+++ applestore-front/src/main/java/applestore/front/catalog/service/CatalogServiceImpl.java
@@ -2,17 +2,13 @@ package applestore.front.catalog.service;
 
 import applestore.domain.catalog.entity.DisplayCategory;
 import applestore.domain.catalog.entity.Product;
-import applestore.domain.catalog.repository.ProductIndexSolrRepository;
 import applestore.domain.catalog.repository.ProductJpaRepository;
-import applestore.domain.catalog.solr.ProductIndex;
-import applestore.framework.ApplicationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static applestore.domain.catalog.entity.Product.hasCategory;
@@ -22,40 +18,21 @@ import static applestore.domain.catalog.entity.Product.hasCategory;
  */
 @Service
 public class CatalogServiceImpl implements CatalogService {
+
     @Autowired
-    private ProductIndexSolrRepository produtIndexRepository;
+    private ProductIndexService indexService;
 
     @Autowired
     private ProductJpaRepository productRepository;
 
     @Override
     public Page<Product> findProductList(final DisplayCategory category, Pageable pageRequest) {
-        List<String> idList = getProductIdList(category, pageRequest);
-        List<Product> productList = productRepository.findByProductIdIn(idList);
+        List<String> productIndex = indexService.getProductIndex(category, pageRequest);
 
+        List<Product> productList = productRepository.findByProductIdIn(productIndex);
         long totalCount = productRepository.count(hasCategory(category.getCategoryId()));
 
         return new PageImpl<Product>(productList, pageRequest, totalCount);
     }
 
-    private List<String> getProductIdList(DisplayCategory category, Pageable pageRequest) {
-        if (category == null) {
-            throw new ApplicationException("카테고리 정보가 필수로 필요합니다!");
-        }
-
-        Page<ProductIndex> page = produtIndexRepository.findByCategoryId(category.getCategoryId(), pageRequest);
-        if (page.getContent() == null) {
-            throw new ApplicationException(category.getCategoryId() + "에 해당하는 상품 정보가 한 건도 등록되어 있지 않습니다!");
-        }
-
-        return toProductIdList(page.getContent());
-    }
-
-    private List<String> toProductIdList(List<ProductIndex> list) {
-        List<String> idList = new ArrayList<String>(list.size());
-        for (ProductIndex cp : list) {
-            idList.add(cp.getProductId());
-        }
-        return idList;
-    }
 }
